09f9ef0ef7c45fcc1d10f1d977a93e9878cab09b
victor olvera <hugotb88@gmail.com>
2019-07-15 12:33:21 -0500

    First

---
+++ src/main/java/com/mastermicroservices/rest/webservices/microservicesrest/MicroservicesrestApplication.java
@@ -0,0 +1,13 @@
+package com.mastermicroservices.rest.webservices.microservicesrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesrestApplication.class, args);
+	}
+
+}
