8beaf75248c635bd37888e2157e832c9de27b0f9
victor olvera <hugotb88@gmail.com>
2019-07-19 10:44:19 -0500

    Path Variable example

--- src/main/java/com/mastermicroservices/rest/webservices/microservicesrest/controllers/HelloWorldController.java
+++ src/main/java/com/mastermicroservices/rest/webservices/microservicesrest/controllers/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.mastermicroservices.rest.webservices.microservicesrest.controllers;
 
 import com.mastermicroservices.rest.webservices.microservicesrest.pojos.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,6 +23,12 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return  new HelloWorldBean("Hello Hugo!");
+    }
+
+    //With a Path Variable
+    @GetMapping(path="/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return  new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 
