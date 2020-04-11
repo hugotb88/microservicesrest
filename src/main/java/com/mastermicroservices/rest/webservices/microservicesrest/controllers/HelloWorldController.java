cc851515d8e3e809893ca4346417d91d0e0daab2
Hugo Olvera <hugotb88@gmail.com>
2020-04-11 16:28:04 -0500

    Removed Bean for set basename for "messages", added configuration in application.properties, Modified Bean for LocaleResolver, used AcceptHeaderLocaleResolver to get the language from the header directly, modified Controller to get the language from LocaleContextHolder

--- src/main/java/com/mastermicroservices/rest/webservices/microservicesrest/controllers/HelloWorldController.java
+++ src/main/java/com/mastermicroservices/rest/webservices/microservicesrest/controllers/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.mastermicroservices.rest.webservices.microservicesrest.controllers;
 import com.mastermicroservices.rest.webservices.microservicesrest.pojos.HelloWorldBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -40,8 +41,7 @@ public class HelloWorldController {
     }
 
     @GetMapping(path="/hello-world-internationalized")
-    public String helloWorldInternationalized(
-            @RequestHeader(name = "Accept-Language")Locale locale){
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
