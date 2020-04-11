20062a7c9f2f41228e36e93374f4109e2e70cfab
Hugo Olvera <hugotb88@gmail.com>
2020-04-11 16:11:26 -0500

    Added I18in Internationalization to a REST using properties file and ResourceBundleMessageSource as bean in app configurations to search in properties files.  LocaleResolver to set a default one, in Controller add a Accept-Language header.

--- src/main/java/com/mastermicroservices/rest/webservices/microservicesrest/controllers/HelloWorldController.java
+++ src/main/java/com/mastermicroservices/rest/webservices/microservicesrest/controllers/HelloWorldController.java
@@ -1,9 +1,14 @@
 package com.mastermicroservices.rest.webservices.microservicesrest.controllers;
 
 import com.mastermicroservices.rest.webservices.microservicesrest.pojos.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 /**
  * @author Victor Hugo Olvera Cruz
@@ -12,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 //Controller
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path="/hello-world")
     public String helloWorld(){
@@ -31,5 +39,9 @@ public class HelloWorldController {
         return  new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
-
+    @GetMapping(path="/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language")Locale locale){
+        return messageSource.getMessage("good.morning.message", null, locale);
+    }
 }
