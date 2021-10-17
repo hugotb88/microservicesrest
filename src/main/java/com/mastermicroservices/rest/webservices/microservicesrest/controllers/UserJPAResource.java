3e28f8e2819397a21c706c5bb38e4ff8939a4b10
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2021-10-16 21:08:40 -0500

    Removed UserService from JPA Controller, added POST and DELETE methods using the DAO, id's modified for testing purposes in .sql file,README updated, Lesson 48 Step 32 or the course

--- src/main/java/com/mastermicroservices/rest/webservices/microservicesrest/controllers/UserJPAResource.java
+++ src/main/java/com/mastermicroservices/rest/webservices/microservicesrest/controllers/UserJPAResource.java
@@ -2,15 +2,18 @@ package com.mastermicroservices.rest.webservices.microservicesrest.controllers;
 
 import com.mastermicroservices.rest.webservices.microservicesrest.daos.UserRepository;
 import com.mastermicroservices.rest.webservices.microservicesrest.exception.UserNotFoundException;
+import com.mastermicroservices.rest.webservices.microservicesrest.exception.UserSaveException;
 import com.mastermicroservices.rest.webservices.microservicesrest.pojos.User;
 import com.mastermicroservices.rest.webservices.microservicesrest.services.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,10 +22,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserJPAResource {
-
-    @Autowired
-    private UserDaoService userService;
-
+    
     @Autowired
     private UserRepository userRepository;
 
@@ -51,5 +51,24 @@ public class UserJPAResource {
     }
 
 
+    //Create User
+    @PostMapping("/jpa/users")
+    public ResponseEntity<Object> createUser(@Valid
+                                             @RequestBody User user){
+        User result = userRepository.save(user);
+        if (result == null)
+            throw new UserSaveException("User not saved:" + user.getName());
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId()).toUri(); //Appends in the URI the id of the new User
+        return ResponseEntity.created(location).build();
+    }
+
+    //Delete a single User
+    @DeleteMapping("/jpa/users/{id}")
+    public void deleteUser(@PathVariable Integer id){
+        userRepository.deleteById(id);
+    }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT into user values(1, sysdate(), 'Batman');
-INSERT into user values(2, sysdate(), 'Robin');
-INSERT into user values(3, sysdate(), 'Nightwing'
+INSERT into user values(10001, sysdate(), 'Batman');
+INSERT into user values(10002, sysdate(), 'Robin');
+INSERT into user values(10003, sysdate(), 'Nightwing'
 );
